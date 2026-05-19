f2f1d13c8e6b1a09932ece17fe63ead5088ccfcd
Clemence-Uwiduhaye <uwiduhayeclemence054@gmail.com>
2026-05-19 20:19:30 +0200

    recall html  and js basics

---
+++ Java script/Exercise.js
@@ -0,0 +1,25 @@
+let name = "Clemence";
+name = "Angela";
+let age = 20;
+let country = "Rwanda";
+let Ilike= "True";
+
+
+const PI = 5
+PI = 8 
+console.log(PI)
+
+
+let firstname = "Clemence";
+let user_age = 20;
+let price = 100
+
+typeof "Clemence"   
+console.log(typeof "Clemence")  
+
+
+
+
+let isLoggedIn = True; 
+let isadmin = false;
+
